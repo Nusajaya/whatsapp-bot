df3fb4d2dccc38968e1ad26f88ae52c83654d286
Nusajaya <124270142+Nusajaya@users.noreply.github.com>
2023-02-12 08:51:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 //global.mod = JSON.parse(fs.readFileSync('./src/moderator.json')) 
-global.owner = ['6281284760551', '6281284760551'] // Your Number
+global.owner = ['6282192834347', '6282192834347'] // Your Number
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 
 global.APIs = { // API Prefix
@@ -54,16 +54,16 @@ global.APIKeys = { // APIKey in here
 
 
 // Sticker WM
-global.packname = '@ cuss.aa'
-global.author = '@ _sekhaa'
+global.packname = '@ nusa'
+global.author = '@ nusa'
 
-global.wm = 'chika aka sekha'
+global.wm = 'nusabotz'
 global.wait = '⏳ tunggu permintaan kamu sedang di proses...'
 global.eror = '× Upss Erorr silahkan lapor owner'
 global.benar = '✓'
 global.salah = '×'
 global.dikit = 'hampir benar'
-global.footer = '© sekha'
+global.footer = '© nusa'
 global.lw = 'https://reysekhaa.herokuapp.com/api/textpro/greenhoror?apikey=apirey&text='
 global.reyganz= 'https://reysekhaa.herokuapp.com/api/wallpaper/ukhty?apikey='
 
